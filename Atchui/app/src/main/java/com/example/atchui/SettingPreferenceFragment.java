02212c9993130e8fb022b7d3c75d8e6f72f1abef
sujin22 <aoghksj@naver.com>
2020-05-21 05:46:52 +0900

    SettingActivity에 기간설정, gps수집동의 추가, 진동 삭제, default 값 설정

--- Atchui/app/src/main/java/com/example/atchui/SettingPreferenceFragment.java
+++ Atchui/app/src/main/java/com/example/atchui/SettingPreferenceFragment.java
@@ -28,9 +28,11 @@ public class SettingPreferenceFragment extends PreferenceFragment {
     SharedPreferences.OnSharedPreferenceChangeListener prefListener  = new SharedPreferences.OnSharedPreferenceChangeListener(){
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            //내장 DB가 변했는데, 변한 내용의 key에 대한 각각의 value에 따른 경우
+            //내장 DB가 변했을 때, 변한 내용의 key에 대한 각각의 value에 따른 조건문
+
+            //푸시알림동의
             if(key.equals("alert")){
-                //사용함
+                //동의
                 if(prefs.getBoolean("alert", false)){
                     if(getActivity() != null){
                         Toast.makeText(getActivity(), getString(R.string.alert_message_y), Toast.LENGTH_SHORT).show();
@@ -38,7 +40,7 @@ public class SettingPreferenceFragment extends PreferenceFragment {
                         boolean now_value = true; //DB테스트를 위해 임시로 담아놓음
                     }
                 }
-                //사용안함
+                //비동의
                 else{
                     if(getActivity() != null){
                         Toast.makeText(getActivity(),getString(R.string.alert_message_n), Toast.LENGTH_SHORT).show();
@@ -48,45 +50,27 @@ public class SettingPreferenceFragment extends PreferenceFragment {
                 }
             }
 
+            //푸시 알림 사운드 설정
             if(key.equals("sound")){
-                //사용함
+                //사용함(사운드)
                 if(prefs.getBoolean("sound", false)){
                     if(getActivity() != null){
                         Toast.makeText(getActivity(), getString(R.string.sound_message_y), Toast.LENGTH_SHORT).show();
                         //TODO: sound on일 때 동작
-                        boolean now_value = false;//DB테스트를 위해 임시로 담아놓음
+                        boolean now_value = true;//DB테스트를 위해 임시로 담아놓음
                     }
                 }
-                //사용안함
+                //사용안함(진동)
                 else{
                     if(getActivity() != null){
                         Toast.makeText(getActivity(),getString(R.string.sound_message_n), Toast.LENGTH_SHORT).show();
                         //TODO: sound off일 때 동작
-                        boolean now_value = true;//DB테스트를 위해 임시로 담아놓음
+                        boolean now_value = false;//DB테스트를 위해 임시로 담아놓음
                     }
                 }
             }
 
-
-            if(key.equals("vibration")){
-                //사용함
-                if(prefs.getBoolean("vibration", false)){
-                    if(getActivity() != null){
-                        Toast.makeText(getActivity(), getString(R.string.vibration_message_y), Toast.LENGTH_SHORT).show();
-                        //TODO: vibration on일 때 동작
-                        boolean now_value = true;//DB테스트를 위해 임시로 담아놓음
-                    }
-                }
-                //사용안함
-                else{
-                    if(getActivity() != null){
-                        Toast.makeText(getActivity(),getString(R.string.vibration_message_n), Toast.LENGTH_SHORT).show();
-                        //TODO: vibration off일 때 동작
-                        boolean now_value = false; //DB테스트를 위해 임시로 담아놓음
-                    }
-                }
-            }
-
+            //반경 설정
             if(key.equals("range")){
                 if(getActivity() != null){
                     int now_value = sharedPreferences.getInt(key,0); //현재 값 받아옴
@@ -97,6 +81,40 @@ public class SettingPreferenceFragment extends PreferenceFragment {
                     //summary에 사용자 설정 값 Set
                     connectionPref.setSummary(new_summary);
 
+                }
+            }
+
+            //기간 설정
+            if(key.equals("range")){
+                if(getActivity() != null){
+                    int now_value = sharedPreferences.getInt(key,0); //현재 값 받아옴
+                    Resources res = getResources();
+                    String new_summary = String.format(res.getString(R.string.period_summary),now_value); //format의 %d에 현재 값 넣기
+
+                    Preference connectionPref = findPreference(key);
+                    //summary에 사용자 설정 값 Set
+                    connectionPref.setSummary(new_summary);
+
+                }
+            }
+
+            //GPS 수집 동의
+            if(key.equals("gps")){
+                //동의
+                if(prefs.getBoolean("vibration", false)){
+                    if(getActivity() != null){
+                        Toast.makeText(getActivity(), getString(R.string.gps_message_y), Toast.LENGTH_SHORT).show();
+                        //TODO: gps on일 때 동작
+                        boolean now_value = true;//DB테스트를 위해 임시로 담아놓음
+                    }
+                }
+                //비동의
+                else{
+                    if(getActivity() != null){
+                        Toast.makeText(getActivity(),getString(R.string.gps_message_n), Toast.LENGTH_SHORT).show();
+                        //TODO: gps off일 때 동작
+                        boolean now_value = false; //DB테스트를 위해 임시로 담아놓음
+                    }
                 }
             }
         }
