ec7130ff9ee9ba7533436d570f7bbe506f5ea97a
sujin22 <aoghksj@naver.com>
2020-05-22 20:34:29 +0900

    background toast, 푸시알림 발생 테스트 주석처리(이제 toast 안 뜰거임)

--- Atchui/app/src/main/java/com/example/atchui/BackgroundService.java
+++ Atchui/app/src/main/java/com/example/atchui/BackgroundService.java
@@ -65,56 +65,56 @@ public class BackgroundService extends Service {
 
             //TODO: GPS 서버 전송, 동선 분석 및 결과 전송
 
-                notificationManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
-
-            Intent intent = new Intent(BackgroundService.this, MainActivity.class);
-            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP ); //
-
-            PendingIntent pendingIntent;//notification을 상태알림창에 띄울 intent
-            pendingIntent = PendingIntent.getActivity(BackgroundService.this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
-                String channelId = "my_notification";
-                String channelName = "n_channel";
-                String channelDescription = "description";
-
-                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
-                channel.setDescription(channelDescription);
-
-                assert notificationManager != null;
-                notificationManager.createNotificationChannel(channel);
-
-//                //채널에 대한 각종 설정
-//                channel.enableLights(true);
-//                channel.setLightColor(Color.RED);
-//                channel.enableVibration(true);
-//                channel.setVibrationPattern(new long[]{100, 200, 300});
-//                Notifi_M.createNotificationChannel(channel);
-            }
-
-            //channel이 등록된 builder(notification 내용 설정 위해 Builder 만들어줌)
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(BackgroundService.this,"my_notification")
-                    .setSmallIcon(R.drawable.btn_notification) //아이콘
-                    .setContentTitle("Title") //알림 제목
-                    .setContentText("ContentText") //알림 내용
-                    .setTicker("알림!!")  //상태바에 표시될 한 줄 출력
-                    .setSound(soundUri)//소리추가
-                    .setColor(Color.parseColor("#ffffff"))
-                    .setContentIntent(pendingIntent)
-                    .setDefaults(Notification.DEFAULT_ALL)
-                    .setOnlyAlertOnce(true) //알림 소리를 한번만 내도록
-                    .setAutoCancel(true) //확인하면 자동으로 알림이 제거 되도록
-//                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//                    .setChannelId("my_notification")
-                    ;
-
-
-            int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
-            notificationManager.notify(m, notificationBuilder.build());
+//            notificationManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
+//
+//            Intent intent = new Intent(BackgroundService.this, MainActivity.class);
+//            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP ); //
+//
+//            PendingIntent pendingIntent;//notification을 상태알림창에 띄울 intent
+//            pendingIntent = PendingIntent.getActivity(BackgroundService.this,0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//            Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ) {
+//                String channelId = "my_notification";
+//                String channelName = "n_channel";
+//                String channelDescription = "description";
+//
+//                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+//                channel.setDescription(channelDescription);
+//
+//                assert notificationManager != null;
+//                notificationManager.createNotificationChannel(channel);
+//
+////                //채널에 대한 각종 설정
+////                channel.enableLights(true);
+////                channel.setLightColor(Color.RED);
+////                channel.enableVibration(true);
+////                channel.setVibrationPattern(new long[]{100, 200, 300});
+////                Notifi_M.createNotificationChannel(channel);
+//            }
+//
+//            //channel이 등록된 builder(notification 내용 설정 위해 Builder 만들어줌)
+//            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(BackgroundService.this,"my_notification")
+//                    .setSmallIcon(R.drawable.btn_notification) //아이콘
+//                    .setContentTitle("Title") //알림 제목
+//                    .setContentText("ContentText") //알림 내용
+//                    .setTicker("알림!!")  //상태바에 표시될 한 줄 출력
+//                    .setSound(soundUri)//소리추가
+//                    .setColor(Color.parseColor("#ffffff"))
+//                    .setContentIntent(pendingIntent)
+//                    .setDefaults(Notification.DEFAULT_ALL)
+//                    .setOnlyAlertOnce(true) //알림 소리를 한번만 내도록
+//                    .setAutoCancel(true) //확인하면 자동으로 알림이 제거 되도록
+////                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+////                    .setChannelId("my_notification")
+//                    ;
+//
+//
+//            int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
+//            notificationManager.notify(m, notificationBuilder.build());
 
             //토스트 띄우기
-            Toast.makeText(BackgroundService.this, "뜸?", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(BackgroundService.this, "안뇽", Toast.LENGTH_SHORT).show();
         }
     };
 }
