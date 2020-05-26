dc1729558e18492bf02fb79790d2d8d3de8346e2
sujin22 <aoghksj@naver.com>
2020-05-26 16:16:58 +0900

    notification_list 만드는 중

---
+++ Atchui/app/src/main/java/com/example/atchui/NotificationListActivity.java
@@ -0,0 +1,14 @@
+package com.example.atchui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_list);
+    }
+}
