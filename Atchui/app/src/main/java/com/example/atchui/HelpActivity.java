b9dff4302a531f49ff7d2e01b5074b8f0d7bef38
chaeso7279 <chaeso7279@gmail.com>
2020-05-28 02:25:57 +0900

    android.support -> androidx 로 바꿈

--- Atchui/app/src/main/java/com/example/atchui/HelpActivity.java
+++ Atchui/app/src/main/java/com/example/atchui/HelpActivity.java
@@ -1,7 +1,7 @@
 package com.example.atchui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
