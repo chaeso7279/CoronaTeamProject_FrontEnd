2cdf9f0d12a00e38effd329cec26a4c5e621454b
chaeso7279 <chaeso7279@gmail.com>
2020-05-28 03:44:31 +0900

    아이템 및 어댑터 추가함 / 자기 전 커밋

--- Atchui/app/src/main/java/com/example/atchui/HelpActivity.java
+++ Atchui/app/src/main/java/com/example/atchui/HelpActivity.java
@@ -1,15 +1,49 @@
 package com.example.atchui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class HelpActivity extends AppCompatActivity {
+
+    private Help_RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        Initialize();
+        setData();
+    }
+
+    private void Initialize() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView_Help);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new Help_RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setData() {
+        HelpData data = new HelpData();
+        data.setQuestion("질문1");
+        data.setContent("답변1");
+        data.setResID(R.drawable.ic_logo);
+
+        adapter.addItem(data);
+
+        data = new HelpData();
+        data.setQuestion("질문2");
+        data.setContent("답변2");
+        data.setResID(R.drawable.ic_logo);
+
+        adapter.addItem(data);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ Atchui/app/src/main/java/com/example/atchui/HelpData.java
@@ -0,0 +1,17 @@
+package com.example.atchui;
+
+public class HelpData {
+
+    private String strQuestion;
+    private String strContent;
+    private int iResID;
+
+    public String getQuesiton() { return strQuestion; }
+    public String getContent() { return strContent; }
+    public int getResID() { return iResID; }
+
+    public void setQuestion(String strQuestion) { this.strQuestion = strQuestion; }
+    public void setContent(String strContent) { this.strContent = strContent; }
+    public void setResID(int iResID) { this.iResID = iResID; }
+
+}
