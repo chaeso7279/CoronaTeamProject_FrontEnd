4f5d3736fb40ce86473fe797ac7cdb869b1f4df9
sujin22 <aoghksj@naver.com>
2020-06-08 04:24:33 +0900

    resultActivity

--- Atchui/app/src/main/java/com/example/atchui/CurrentResultActivity.java
+++ Atchui/app/src/main/java/com/example/atchui/CurrentResultActivity.java
@@ -22,6 +22,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.atchui.network.DataManager;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -43,165 +44,57 @@ public class CurrentResultActivity extends FragmentActivity implements OnMapRead
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     private static final String TAG = "CurrentResultActivity";
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
-    private static final float DEFAULT_ZOOM = 15f;
 
-    //widgets
-    private ImageView mGps;
+    TextView user_location;
+    TextView user_datetime;
+    TextView cnf_location;
+    TextView cnf_datetime;
+    TextView info_infectcase;
+    TextView info_cnfdate;
+    TextView info_province;
+    TextView info_isofacility;
 
-    //vars
-    private Boolean mLocationPermissionsGranted =false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_result);
 
+        user_location = (TextView)findViewById(R.id.userLocation);
+        user_datetime = (TextView)findViewById(R.id.userDatetime);
+        cnf_location = (TextView)findViewById(R.id.cnfLocation);
+        cnf_datetime = (TextView)findViewById(R.id.cnfDatetime);
+        info_infectcase = (TextView)findViewById(R.id.info_infectCase);
+        info_cnfdate = (TextView)findViewById(R.id.info_cnfdate);
+        info_province = (TextView)findViewById(R.id.info_province);
+        info_isofacility = (TextView)findViewById(R.id.info_isoFacility);
+
+        /*선택된 알림 정보 가져오기*/
         Intent intent = getIntent();
-//        int lstIndex = intent.getExtras.getString("lstIndex");
-        getLocationPermission();
+        int lstIndex = intent.getExtras().getInt("lstIndex");
+
+        getSelectedNotiData(lstIndex);
+
+        /*google map*/
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_currentResult);
+                .findFragmentById(R.id.map_pathResult);
+    //        mapFragment.getMapAsync(PathResultActivity.this);
+    }
 
-        MapFragment mapFragment1 = (MapFragment) getFragmentManager().findFragmentById(R.id.map_currentResult);
-        mapFragment.getMapAsync(CurrentResultActivity.this);
-
-        mGps = (ImageView) findViewById(R.id.ic_gps);
-        mapFragment.getMapAsync(this);
+    private void getSelectedNotiData(int index){
+       // DataManager.getInstance().lstAnal.get(index);
+        cnf_location.setText(
+                DataManager.getInstance().lstAnal.get(index).m_locationName
+        );
 
     }
 
-    private void init(){
-        Log.d(TAG,"init: initializing");
-
-        mGps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked gps icon");
-                getDeviceLocation();
-            }
-        });
-    }
+    //google map
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
         Toast.makeText(this,"map is Ready",Toast.LENGTH_SHORT).show();
         Log.d(TAG,"onMapReady : map is ready");
         mMap = googleMap;
-
-        if(mLocationPermissionsGranted){
-            getDeviceLocation();
-
-            //자신의 위치 나타내기
-            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED&&ActivityCompat.checkSelfPermission(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                return;
-            }
-            mMap.setMyLocationEnabled(true);
-            //자기 위치 찾는거 지운다
-            mMap.getUiSettings().setMyLocationButtonEnabled(false);
-            init();
-        }
-    }
-
-    //현위치 가져오기
-    private void getDeviceLocation(){
-        Log.d(TAG,"getDeviceLocation: getting the current devices location");
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        try{
-            if(mLocationPermissionsGranted){
-                final Task location = mFusedLocationProviderClient.getLastLocation();
-                location.addOnCompleteListener(new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if(task.isSuccessful()){
-                            Log.d(TAG,"onComplete: found location!");
-                            Location currentLocation = (Location)task.getResult();
-
-                            moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),
-                                    DEFAULT_ZOOM,
-                                    "My Location");
-                        }else{
-                            Log.d(TAG,"onComplete: current location is null");
-                            Toast.makeText(CurrentResultActivity.this,"unable to get current location",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            }
-        }catch(SecurityException e){
-            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
-        }
-    }
-
-
-    private void moveCamera(LatLng latLng,float zoom, String title){
-        Log.d(TAG, "moveCamera: moving the camera to: lat: "+ latLng.latitude + ", lng: "+ latLng.longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
-
-        if(!title.equals("My Location")){
-            MarkerOptions options = new MarkerOptions()
-                    .position(latLng)
-                    .title(title);
-            mMap.addMarker(options);
-        }
-    }
-
-    private void initMap(){
-        Log.d(TAG,"initMap:initializing map");
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_currentResult);
-        mapFragment.getMapAsync(CurrentResultActivity.this);
-    }
-
-    //위치정보 동의
-    private void getLocationPermission(){
-        Log.d(TAG,"getLocationPermission: getting location permission");
-        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION};
-
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                    COURSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                mLocationPermissionsGranted = true;
-                initMap();
-            }else{
-                ActivityCompat.requestPermissions(this,
-                        permissions,
-                        LOCATION_PERMISSION_REQUEST_CODE);
-            }
-        }else{
-            ActivityCompat.requestPermissions(this,
-                    permissions,
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
-    }
-
-
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG,"onRequestPermissionsResult: called");
-        mLocationPermissionsGranted=false;
-
-        switch (requestCode){
-            case LOCATION_PERMISSION_REQUEST_CODE:{
-                if(grantResults.length > 0){
-                    for(int i = 0; i < grantResults.length;i++){
-                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
-                            mLocationPermissionsGranted = false;
-                            Log.d(TAG,"onRequestPermissionsResult: failed");
-                            return;
-                        }
-                    }
-                    Log.d(TAG,"onRequestPermissionsResult: permission granted");
-
-                    mLocationPermissionsGranted = true;
-                    //initialize our map
-                    initMap();
-                }
-            }
-        }
     }
 }
